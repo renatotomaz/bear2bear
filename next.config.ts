5f6ef687fa693a6a9bea41d8be1300bd7f8096aa
renatotomaz <renatotomaz@gmail.com>
2025-10-30 18:39:15 +0000

    Log keys

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,8 @@
 import type { NextConfig } from "next";
 
+console.log('🔎 NEXT_PUBLIC_SUPABASE_URL:', process.env.NEXT_PUBLIC_SUPABASE_URL)
+console.log('🔎 NEXT_PUBLIC_SUPABASE_ANON_KEY:', process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY)
+
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
