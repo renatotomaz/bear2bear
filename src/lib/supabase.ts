6e92508918484c7fe617310766ca595c19fd180a
renatotomaz <renatotomaz@gmail.com>
2025-10-30 18:50:11 +0000

    Alteração de key

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,8 +1,10 @@
 import { createBrowserClient } from '@supabase/ssr'
 
+const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY?.replace(/^PREFIX_/, '')
+
 export const createClient = () => {
   return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    key! // <-- usa a chave ajustada
   )
 }
